66010c52d3ef825d5d6901be77ba18579af6a699
ShugoYabuno <shugo.ybn@gmail.com>
2023-03-22 21:08:16 +0900

    fix error

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "break-out-room-tool",
+  base: "/break-out-room-tool",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
